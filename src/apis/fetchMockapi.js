2851f4604e3186306616ff38d4e4b2fd4d1cb9e9
Klyment Kim <klim86kr@gmail.com>
2023-10-08 12:21:31 +0300

    add fetch

--- src/apis/fetchMockapi.js
+++ src/apis/fetchMockapi.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://pixabay.com/api/";
-const API_KEY = "37187693-cb92e988afefb94434f725c21";
-export const IMAGE_TYPE = "photo";
-export const ORIENTATION = "horizontal";
+axios.defaults.baseURL = "https://65216e3da4199548356d33a1.mockapi.io/api/1/";
 export const quantityPage = 8;
 
-const fetchMockapi = async (inputValue, page = 1) => {
+const fetchMockapi = async (page = 1) => {
   try {
     const response = await axios.get(
-      `?key=${API_KEY}&q=${inputValue}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&page=${page}&${quantityPage}`
+      `adverts?quantityPage=${quantityPage}&page=${page}`
     );
     return response.data;
   } catch (error) {
@@ -18,9 +15,3 @@ const fetchMockapi = async (inputValue, page = 1) => {
 };
 
 export default fetchMockapi;
-
-// Функция для нормализации массива изображений
-export const normalizedImages = (imagesArray) =>
-  imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
-    return { id, tags, webformatURL, largeImageURL };
-  });
