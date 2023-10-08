0d6818f7e69975297ba70a568f3b726df158007c
Klyment Kim <klim86kr@gmail.com>
2023-10-09 02:51:07 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-// import { Provider } from "react-redux";
-// import store from "./redux/store";
-// import {  persistor } from "./redux/store";
-// import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <Provider store={store}>
-    //  {/* <PersistGate persistor={persistor}> */}
+  <Provider store={store}>
+     <PersistGate persistor={persistor}>
       <BrowserRouter basename="/car-rental">
         <App />
       </BrowserRouter>
-    // {/* </PersistGate> */}
-  // </Provider>
+     </PersistGate>
+  </Provider>
 );
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
