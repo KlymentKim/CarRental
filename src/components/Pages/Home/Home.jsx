bee5dbed23ee942c6df5d656ccfdaffb86468497
Klyment Kim <klim86kr@gmail.com>
2023-10-09 00:31:30 +0300

    commit

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import css from "./Home.module.css";
+import MovingText from "../../../helpers/MovingText/AnimationChain"
 // import backgroundImg from "../../../assets/img/rentcar_bg.jpg";
 
 const Home = () => {
   return (
-    <div className={css.backgroundImg} >
-    <h1 className={css.header}>Welcome to the Car-Rental service</h1>
-  </div>
+    <article className={css.backgroundImg} >
+   <MovingText />
+    {/* <h1 className={css.title}>Welcome to the Car-Rental service</h1> */}
+   </article>
   )
 };
 
---
+++ src/helpers/MovingText/AnimationChain.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import MovingComponent from "react-moving-text";
+import css from "../../components/Pages/Home/Home.module.css";
+
+const AnimationsForChaining = ["popIn"];
+
+const AnimationChain = () => {
+  return (
+    <div className={css.title}>
+      <MovingComponent
+        type={AnimationsForChaining}
+        duration="3000ms"
+        delay="0s"
+        direction="normal"
+        timing="ease"
+        iteration="1"
+        fillMode="none"
+      >
+        Welcome to a Car-Rental service
+      </MovingComponent>
+    </div>
+  );
+};
+export default AnimationChain;
