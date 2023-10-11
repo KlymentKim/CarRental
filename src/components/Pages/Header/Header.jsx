5ea1314f47d34d779c91ce333cdb1e61c7697c33
Klyment Kim <klim86kr@gmail.com>
2023-10-12 00:41:18 +0300

    commit

--- src/components/Pages/Header/Header.jsx
+++ src/components/Pages/Header/Header.jsx
@@ -5,20 +5,30 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className={css.wrapper_header}>
-      <div className={css.header}>
+    <div className={css.header}>
+      <div>
         <NavLink className={css.logo_header} to="/Home">
           <div className={css.wrap}>
             <p className={css.gradient_text}>Car-Rental</p>
           </div>
-          </NavLink> 
-        <nav className={css.nav_header}>
-          <ul className={css.header_list}>
-            <li className={css.header_item}><NavLink to="/Home">Home</NavLink></li>
-            <li className={css.header_item}><NavLink to="/Catalog">Catalog</NavLink></li>
-            <li className={css.header_item}><NavLink to="/Favourites">Favourites</NavLink></li>
-          </ul>
-        </nav>
+        </NavLink>
+      </div>
+      <div>
+        <div className={css.div_nav}>
+          <nav className={css.nav_header}>
+            <ul className={css.header_list}>
+              <li className={css.header_item}>
+                <NavLink to="/Home">Home</NavLink>
+              </li>
+              <li className={css.header_item}>
+                <NavLink to="/Catalog">Catalog</NavLink>
+              </li>
+              <li className={css.header_item}>
+                <NavLink to="/Favourites">Favourites</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
